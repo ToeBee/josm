b2e03a5a32ac17d860969d4b9fef5e5238c92133
Toby Murray <toby.murray@gmail.com>
2014-08-26 23:42:04 -0500

    Adding equals and hashCode methods to Note

--- src/org/openstreetmap/josm/data/notes/Note.java
+++ src/org/openstreetmap/josm/data/notes/Note.java
@@ -101,4 +101,26 @@ public class Note {
         this.latLon = note.latLon;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    /** Compares notes by OSM ID */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Note other = (Note) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
 }
